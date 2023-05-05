669711e2a836840cca6081e57d010651509e763a
PrashantJangam13 <prashantjangam13062000@gmail.com>
2023-05-05 10:21:35 +0530

    commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { CsrListComponent } from './viewcsrdata/csr-list/csr-list.component';
+import { CsrformComponent } from './addcunsumer/csrform/csrform.component';
+import { RegisterformComponent } from './usermodule/registerform/registerform.component';
+import { LoginComponent } from './usermodule/login/login.component';
+import { WelcomeUserComponent } from './usermodule/welcomeuser/welcomeuser.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{path:'',component:WelcomeComponent},{path:'csrlist',component:CsrListComponent},{path:'csrform',component:CsrformComponent},{path:'updateCsr/:id',component:CsrformComponent},{path:'register',component:RegisterformComponent},{path:'login',component:LoginComponent},{path:'welcome',component:WelcomeUserComponent},{path:'logout',component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CsrListComponent } from './viewcsrdata/csr-list/csr-list.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { CsrformComponent } from './addcunsumer/csrform/csrform.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RegisterformComponent } from './usermodule/registerform/registerform.component';
+import { LoginComponent } from './usermodule/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsrListComponent,
+    WelcomeComponent,
+    CsrformComponent,
+    RegisterformComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
